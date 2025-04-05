fc5229f1cbdce04cd95a5220517e3e410765026f
William Roger <williamroger.frontend@gmail.com>
2025-04-05 01:54:41 -0300

    project started

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('🚀 Server is running on port 3000...');
+});
