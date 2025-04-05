e736d7410f6024b1ec81d1a412be0ab59b27b244
William Roger <williamroger.frontend@gmail.com>
2025-04-05 02:13:35 -0300

    adicionado nodemon

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  response.send('Hello World!');
+  response.send('Change the World!');
 });
 
 app.listen(3000, () => {
