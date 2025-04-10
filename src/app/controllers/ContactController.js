5a385697816d7e43d485cb7bc172d315154e9991
William Roger <williamroger.frontend@gmail.com>
2025-04-10 17:58:29 -0300

    adicionada a rota de edicao de contato

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -43,8 +43,36 @@ class ContactController {
   }
 
   // este método serve para atualizar um registro específico
-  update() {
+  async update(request, response) {
+    const { id } = request.params;
+    const { name, email, phone, category_id } = request.body;
+    const contactExists = await ContactsRepository.findById(id);
+    const contactByEmail = await ContactsRepository.findByEmail(email);
 
+    if (!contactExists) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    if (contactByEmail && contactByEmail.id !== id) {
+      // se o contato existe e o id do contato que está sendo atualizado é diferente do id do contato que já existe
+      // ou seja, se o contato que está sendo atualizado já existe
+      // e o id é diferente do id do contato que já existe
+      // então retorna um erro
+      return response.status(400).json({ error: 'This email is already registered' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name,
+      email,
+      phone,
+      category_id,
+    });
+
+    return response.json(contact);
   }
 
   // este método serve para deletar um registro específico
--- src/routes.js
+++ src/routes.js
@@ -7,4 +7,6 @@ router.get('/contacts', ContactController.index); // Lista todos os contatos
 router.get('/contacts/:id', ContactController.show); // Mostra um contato específico
 router.delete('/contacts/:id', ContactController.delete); // Deleta um contato específico
 router.post('/contacts', ContactController.store); // Cria um novo contato
+router.put('/contacts/:id', ContactController.update); // Atualiza um contato específico
+
 module.exports = router;
