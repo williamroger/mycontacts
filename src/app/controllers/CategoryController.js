95df96ba0ac44b8fa3ead0eef6526cb0afea5125
William Roger <williamroger.frontend@gmail.com>
2025-04-26 12:51:01 -0300

    módulo category adicionadas rotas para editar e excluir

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -6,6 +6,17 @@ class CategoryController {
     return response.status(200).json(categories);
   }
 
+  async show(request, response) {
+    const { id } = request.params;
+    const category = await CategoryRepository.findById(id);
+
+    if (!category) {
+      return response.status(404).json({ error: 'Category not found' });
+    }
+
+    response.status(200).json(category);
+  }
+
   async store(request, response) {
     const { name } = request.body;
 
@@ -16,6 +27,37 @@ class CategoryController {
     const category = await CategoryRepository.create({ name });
 
     return response.status(201).json(category);
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const { name } = request.body;
+
+    const categoryExists = await CategoryRepository.findById(id);
+
+    if (!categoryExists) {
+      return response.status(404).json({ error: 'Category not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    if (categoryExists.name === name) {
+      return response.status(400).json({ error: 'This name is already registered' });
+    }
+
+    const newCategory = await CategoryRepository.update(id, { name });
+
+    response.status(200).json(newCategory);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await CategoryRepository.delete(id);
+
+    response.sendStatus(204);
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,8 @@ router.post('/contacts', ContactController.store); // Cria um novo contato
 router.put('/contacts/:id', ContactController.update); // Atualiza um contato específico
 
 router.get('/categories', CategoryController.index); // Lista todas as categorias
+router.get('/categories/:id', CategoryController.show); // Mostra uma categoria específica
 router.post('/categories', CategoryController.store); // Cria uma nova categoria
-
+router.put('/categories/:id', CategoryController.update); // Atualiza uma categoria específica
+router.delete('/categories/:id', CategoryController.delete); // Deleta uma categoria específica
 module.exports = router;
