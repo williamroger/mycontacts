d904b1473f076034dccb688845bfcb1adb27f041
William Roger <williamroger.frontend@gmail.com>
2025-04-27 12:12:46 -0300

    utilizando Error Handler para tratar erros nas rotas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+require('express-async-errors'); // Middleware para tratamento de erros assíncronos
+
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json()); // Middleware para fazer o parse do JSON no body da requisição
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log('###### Error Handler ######');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => {
   console.log('🚀 Server is running on port 3000...');
